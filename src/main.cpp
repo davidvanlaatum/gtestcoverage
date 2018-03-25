037ad73baadf8ba352b48ca7c7b4290f3104ac13
David van Laatum <david@vanlaatum.id.au>
2018-03-25 18:35:41 +1030

    clang coverage

--- src/main.cpp
+++ src/main.cpp
@@ -4,3 +4,23 @@ int main( int argc, char **argv ) {
   testing::coverage::cli::CLI cli;
   return cli.run( argc, argv );
 }
+
+extern "C" void main_coverage_dump() {
+}
+
+extern "C" void main_coverage_reset() {
+}
+
+extern "C" void unit_coverage_dump() {
+}
+
+extern "C" void unit_coverage_reset() {
+}
+
+extern "C" const char *main_driver_name() {
+  return "null";
+}
+
+extern "C" const char *unit_driver_name() {
+  return "null";
+}
